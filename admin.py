ac0d09766e56b1513e51c10eeef332761b8c365b
Deepak Prajapat <deepakprajapat7067@gmail.com>
2025-08-25 17:48:05 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,7 +1,27 @@
 # admin_app/admin.py
 import streamlit as st
 import pandas as pd
-from Shared.db import get_connection
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import hashlib
+
+DB_CONFIG = {
+    "user": "postgres.jsjlthhnrtwjcyxowpza",
+    "password": "@Deep7067",
+    "host": "aws-1-ap-south-1.pooler.supabase.com",
+    "port": "6543",
+    "dbname": "postgres",
+    "sslmode": "require"
+}
+
+def get_connection():
+    return psycopg2.connect(**DB_CONFIG)
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
 
 st.set_page_config(page_title="Admin Panel", layout="wide")
 
